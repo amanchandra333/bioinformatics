9bffd81353543bc68dd4753cde78517cfda93f57
amanchandra333 <amanchandra333@gmail.com>
2018-01-23 21:25:18 +0530

    bug fixes

--- aa_rna.c
+++ aa_rna.c
@@ -3,9 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#define token_count 12					//Number of Columns
+#include <stdbool.h>
 
-char **strsplit(const char* pdb_line, const char* delim) {
+struct pose {
+   float x;
+   float y;
+   float z;
+   int atom;
+};
+
+char **strsplit(const char* pdb_line, const char* delim, int * token_count) {
     char *s = strdup(pdb_line);      			//Create a copy of the string
 
     size_t allocated = 1, used =0;
@@ -27,70 +34,98 @@ char **strsplit(const char* pdb_line, const char* delim) {
     } else {
         tokens = realloc(tokens, used * sizeof(char*));
     }
+    * token_count = used;
     free(s);
     return tokens;
 }
+
 int main(void) {
     FILE * pdb;
     FILE * pdb2;
-    FILE * aa;
-    FILE * rna;
+    FILE * temp;
     char *line = NULL;
-    char *line2 = NULL;
+    char * line2 = NULL;
     size_t len, len2;
     ssize_t read, read2;
-    char **tokens1 = calloc(20, sizeof(char*));
-    char **tokens2 = calloc(20, sizeof(char*));
+    char **tokens;
+    int token_count;
     double dist;
 
+    pdb2 = fopen("1asy.pdb", "r");								//Check if file is valid
+    if (pdb2 == NULL)
+        exit(EXIT_FAILURE);
     pdb = fopen("1asy.pdb", "r");								//Check if file is valid
     if (pdb == NULL)
         exit(EXIT_FAILURE);
-    pdb2 = fopen("1asy.pdb", "r");                                                               //Check if file is valid
-    if (pdb2 == NULL)
-        exit(EXIT_FAILURE);
 
-    aa = fopen ("aa.pdb","w");
-    fclose (aa);
-    rna = fopen ("rna.pdb","w");
-    fclose (rna);
+    temp = fopen ("b.pdb","w");
+    fclose (temp);
 
-    while ((read = getline(&line, &len, pdb)) != -1) {						//Read one line at a time
-	tokens1 = strsplit(line, " \t\n");
-	if(!strcmp(tokens1[0],"ATOM")){
-	    if(!strcmp(tokens1[4],"A")){
-		while ((read2 = getline(&line2, &len2, pdb2)) != -1) {
-		    tokens2 = strsplit(line2, " \t\n");
-		    //printf("hre3\n");
-		    if(!strcmp(tokens2[0],"ATOM")){
-			if(!strcmp(tokens2[4],"R")){
-			    //printf("calculating distance\n");
-			    dist = sqrt(pow(atof(tokens1[6])-atof(tokens2[6]),2) + 
-				pow(atof(tokens1[7])-atof(tokens2[7]),2) + pow(atof(tokens1[8])-atof(tokens2[8]),2));
-			    if(dist <= 4.5){
-				aa = fopen ("aa.pdb","a");
-				fprintf (aa, "%s  %5s  %-3s %3s %s%4s    %8s%8s%8s  %4s %5s           %s\n",    //Write to file
-                        		tokens1[0], tokens1[1], tokens1[2], tokens1[3], tokens1[4], tokens1[5], tokens1[6],
-                        		tokens1[7], tokens1[8], tokens1[9], tokens1[10], tokens1[11]);
-                		fclose (aa);
+    size_t allocated_a = 1, allocated_r=1, used_a =0, used_r=0;
+    bool *check = calloc(20000, sizeof(bool));
+    struct pose *aa = calloc(allocated_a, sizeof(struct pose));
+    struct pose *rna = calloc(allocated_r, sizeof(struct pose));
 
-				rna = fopen ("rna.pdb","a");
-                		fprintf (rna, "%s  %5s  %-3s %3s %s%4s    %8s%8s%8s  %4s %5s           %s\n",    //Write to file
-                        		tokens2[0], tokens2[1], tokens2[2], tokens2[3], tokens2[4], tokens2[5], tokens2[6],
-                        		tokens2[7], tokens2[8], tokens2[9], tokens2[10], tokens2[11]);
-                		fclose (rna);
-			    }
-			}
-		    }
-		    if (tokens2 != NULL)
-                    	free(tokens2);
-		}
-		if (line2 != NULL) free(line2);
-	    }
+    while ((read = getline(&line, &len, pdb)) != -1) {						                        //Read one line at a time
+    	tokens = strsplit(line, " \t\n", &token_count);
+    	if(!strcmp(tokens[0],"ATOM")){
+            if(!strcmp(tokens[4],"A")){
+                if (used_a == allocated_a) {			                                            //Increase allocated space if needed
+                    allocated_a *= 2;
+                    aa= realloc(aa, allocated_a * sizeof(struct pose));
+                }
+                aa[used_a].x= atof(tokens[6]);
+                aa[used_a].y= atof(tokens[7]);
+                aa[used_a].z= atof(tokens[8]);
+                aa[used_a++].atom = atoi(tokens[1]);
+            }
+            if(!strcmp(tokens[4],"R")){
+                if (used_r == allocated_r) {			                                            //Increase allocated space if needed
+                    allocated_r *= 2;
+                    rna = realloc(rna, allocated_r * sizeof(struct pose));
+                }
+                rna[used_r].x= atof(tokens[6]);
+                rna[used_r].y= atof(tokens[7]);
+                rna[used_r].z= atof(tokens[8]);
+                rna[used_r++].atom = atoi(tokens[1]);
+            }
         }
-        if (tokens1 != NULL)
-            free(tokens1);
+        if (tokens != NULL)
+            free(tokens);
     }
+    aa= realloc(aa, used_a * sizeof(struct pose));
+    rna= realloc(rna, used_r * sizeof(struct pose));
     if (line != NULL) free(line);
+    fclose(pdb);
+
+    int i, j;
+    for(i=0; i<used_a; i++){
+        for(j=0; j<used_r; j++){
+            dist = sqrt(pow(aa[i].x-rna[j].x,2) + pow(aa[i].y-rna[j].y,2) + pow(aa[i].z-rna[j].z,2));
+            if(dist <= 4.5){
+                check[aa[i].atom] = true;
+                check[rna[j].atom] = true;
+            }
+        }
+    }
+
+    while ((read2 = getline(&line2, &len2, pdb2)) != -1) {						//Read one line at a time
+        tokens = strsplit(line2, " \t\n", &token_count);
+        if(!strcmp(tokens[0],"ATOM") && check[atoi(tokens[1])]){
+            temp = fopen ("b.pdb","a");
+            if(token_count==12)
+                fprintf (temp, "%s  %5s  %-3s %3s %s%4s    %8s%8s%8s  %4s %5s           %s\n",	//Write to file
+                        tokens[0], tokens[1], tokens[2], tokens[3], tokens[4], tokens[5], tokens[6],
+                        tokens[7], tokens[8], tokens[9], tokens[10], tokens[11]);
+            else
+                fprintf (temp, "%s  %5s  %-3s %3s %s%4s    %8s%8s%8s  %10s           %s\n",	//Write to file
+                        tokens[0], tokens[1], tokens[2], tokens[3], tokens[4], tokens[5], tokens[6],
+                        tokens[7], tokens[8], tokens[9], tokens[10]);
+            fclose (temp);
+        }
+        if (tokens != NULL)
+            free(tokens);
+    }
+    if (line2 != NULL) free(line2);
     return EXIT_SUCCESS;
 }
