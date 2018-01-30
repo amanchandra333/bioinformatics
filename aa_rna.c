663a99180e4ea26db41171e2908f3c309a22dabe
amanchandra333 <amanchandra333@gmail.com>
2018-01-30 14:58:43 +0530

    print interacting atoms

--- aa_rna.c
+++ aa_rna.c
@@ -10,6 +10,9 @@ struct pose {
    float y;
    float z;
    int atom;
+   int res;
+   char* atom_name;
+   char* res_name;
 };
 
 char **strsplit(const char* pdb_line, const char* delim, int * token_count) {
@@ -43,6 +46,7 @@ int main(void) {
     FILE * pdb;
     FILE * pdb2;
     FILE * temp;
+    FILE * temp2;
     char *line = NULL;
     char * line2 = NULL;
     size_t len, len2;
@@ -58,8 +62,10 @@ int main(void) {
     if (pdb == NULL)
         exit(EXIT_FAILURE);
 
-    temp = fopen ("b.pdb","w");
+    temp = fopen ("interact.pdb","w");
     fclose (temp);
+    temp2 = fopen ("pymol_interact.pdb","w");
+    fclose (temp2);
 
     size_t allocated_a = 1, allocated_r=1, used_a =0, used_r=0;
     bool *check = calloc(20000, sizeof(bool));
@@ -77,6 +83,9 @@ int main(void) {
                 aa[used_a].x= atof(tokens[6]);
                 aa[used_a].y= atof(tokens[7]);
                 aa[used_a].z= atof(tokens[8]);
+		aa[used_a].res= atof(tokens[5]);
+		aa[used_a].atom_name= strdup(tokens[2]);
+		aa[used_a].res_name= strdup(tokens[3]);
                 aa[used_a++].atom = atoi(tokens[1]);
             }
             if(!strcmp(tokens[4],"R")){
@@ -87,6 +96,9 @@ int main(void) {
                 rna[used_r].x= atof(tokens[6]);
                 rna[used_r].y= atof(tokens[7]);
                 rna[used_r].z= atof(tokens[8]);
+		rna[used_r].res= atof(tokens[5]);
+                rna[used_r].atom_name= strdup(tokens[2]);
+                rna[used_r].res_name= strdup(tokens[3]);
                 rna[used_r++].atom = atoi(tokens[1]);
             }
         }
@@ -105,6 +117,12 @@ int main(void) {
             if(dist <= 4.5){
                 check[aa[i].atom] = true;
                 check[rna[j].atom] = true;
+		temp = fopen ("interact.pdb","a");
+		//fprintf(temp, "%d\n", aa[i].res);				 //Print only residue numbers
+		fprintf (temp, "%s,\t%d,\t%s,\t%d\t-\t%s,\t%d,\t%s,\t%d\t%f\n",  //Write to file
+                        aa[i].atom_name, aa[i].atom, aa[i].res_name, aa[i].res,
+			rna[j].atom_name, rna[j].atom, rna[j].res_name, rna[j].res, dist);
+		fclose (temp);
             }
         }
     }
@@ -112,20 +130,21 @@ int main(void) {
     while ((read2 = getline(&line2, &len2, pdb2)) != -1) {						//Read one line at a time
         tokens = strsplit(line2, " \t\n", &token_count);
         if(!strcmp(tokens[0],"ATOM") && check[atoi(tokens[1])]){
-            temp = fopen ("b.pdb","a");
-            if(token_count==12)
-                fprintf (temp, "%s  %5s  %-3s %3s %s%4s    %8s%8s%8s  %4s %5s           %s\n",	//Write to file
+            temp2 = fopen ("pymol_interact.pdb","a");
+            if(token_count==12) 
+                fprintf (temp2, "%s  %5s  %-3s %3s %s%4s    %8s%8s%8s  %4s %5s           %s\n",	//Write to file
                         tokens[0], tokens[1], tokens[2], tokens[3], tokens[4], tokens[5], tokens[6],
                         tokens[7], tokens[8], tokens[9], tokens[10], tokens[11]);
             else
-                fprintf (temp, "%s  %5s  %-3s %3s %s%4s    %8s%8s%8s  %10s           %s\n",	//Write to file
+                fprintf (temp2, "%s  %5s  %-3s %3s %s%4s    %8s%8s%8s  %10s           %s\n",	//Write to file
                         tokens[0], tokens[1], tokens[2], tokens[3], tokens[4], tokens[5], tokens[6],
                         tokens[7], tokens[8], tokens[9], tokens[10]);
-            fclose (temp);
+            fclose (temp2);
         }
         if (tokens != NULL)
             free(tokens);
     }
     if (line2 != NULL) free(line2);
+
     return EXIT_SUCCESS;
 }
